84c079cc8a1a3cca485ad58531d06b00eee0f3ca
Allison Harris <allison.harris23@gmail.com>
2014-12-15 20:19:17 -0600

    Exercise 3

---
+++ ex3.rb
@@ -0,0 +1,30 @@
+puts "I will now count my chickens:"
+
+# Divide 30 by 6 and then add 25
+puts "Hens #{25 + 30 / 6}"
+
+puts "Roosters #{100 - 25 * 3 % 4}"
+
+puts "Now I will count the eggs:"
+
+# asking a question
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+# evaluating the above question
+puts 3 + 2 < 5 - 7
+
+# printing a string with the answer calculated after
+puts "What is 3 + 2? #{3 + 2}"
+# printing a string with the answer calculated after
+puts "What is 5 - 7? #{5 - 7}"
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+
+# asking a question and evaluating an expression
+puts "Is it greater? #{5 > -2}"
+# asking a question and evaluating an expression
+puts "Is it greater or equal? #{5 >= -2}"
+# asking a question and evaluating an expression
+puts "Is it less or equal? #{5 <= -2}"
