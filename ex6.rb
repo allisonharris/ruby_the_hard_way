8ede1a507f826e1763303fa00963c9d976f65f2d
Allison Harris <allison.harris23@gmail.com>
2014-12-20 15:22:28 -0600

    Commented Ex6 and Created Ex.7

--- ex6.rb
+++ ex6.rb
@@ -1,21 +1,32 @@
+#variable to store number of types of people
 types_of_people = 10
+# variable to store the string about the types of people
 x = "There are #{types_of_people} types of people."
+# variable to store string for binary
 binary = "binary"
+#variable to store string for don't
 do_not = "don't"
+# variable to store string for the different types of people
 y = "Those who know #{binary} and those who #{do_not}."
 
+# print x and y
 puts x
 puts y
 
+# print string about x and y
 puts "I said: #{x}."
 puts "I also said: '#{y}'."
 
+# variable about evaluation of joke
 hilarious = false
 joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
 
+# print evaluation of joke
 puts joke_evaluation
 
+# variables to store string for w and e
 w = "This is the left side of..."
 e = "a string with a right side."
 
+# print strings for w and e
 puts w + e 
