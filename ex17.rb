942c5fc635a26268671211ad6e4746acb1bd9c3b
Allison Harris <allison.harris23@gmail.com>
2015-01-06 22:14:10 -0600

    Cleaning up ex17

--- ex17.rb
+++ ex17.rb
@@ -1,1 +1,8 @@
-File.open(to_file, 'w').write(File.open(from_file).read())
+from_file, to_file = ARGV
+script = $0
+
+input = File.open(from_file); indata = input.read()
+output = File.open(to_file, 'w'); output.write(indata)
+
+output.close()
+input.close()
