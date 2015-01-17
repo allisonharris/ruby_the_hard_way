5bb7a6ad93628a19530873734c8aa8ba14567160
Allison Harris <allison.harris23@gmail.com>
2015-01-17 15:23:01 -0600

    fixed error in line 33

--- ex21.rb
+++ ex21.rb
@@ -30,6 +30,6 @@ puts "Age: #{age}, Height: #{height}, Weight: #{weight}, IQ: #{iq}"
 # A puzzle for the extra credit, type it in anyway.
 puts "Here is a puzzle."
 
-what = add(age, subtract(height, multiply(weight, divide(iq/2))))
+what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
 
 puts "That becomes: #{what}. Can you do it by hand?"
